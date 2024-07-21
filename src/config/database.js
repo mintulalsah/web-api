f90f579f20deb0dc5944c5f22b1dbec000f98052
mintultriazine <mintul@triazinesoft.com>
2024-07-21 11:39:10 +0530

    running we enable pass auth

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
-
 const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
-console.log('db name',dbName);
-main().then(res => console.log("mongodb connected successfully...!!!"))
-main().catch(err => console.log(err));
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+console.log('db name', dbName);
+
 async function main() {
-  const uri = `mongodb://${dbHost}:27017/${dbName}`;
+  const uri = `mongodb://${dbUser}:${dbPassword}@${dbHost}:27017/${dbName}?authSource=${dbName}`;
 
   await mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 2000,
   });
+
+  console.log("mongodb connected successfully...!!!");
 }
+
+main().catch(err => console.log(err));
